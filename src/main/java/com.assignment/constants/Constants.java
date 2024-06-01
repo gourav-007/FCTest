a6a99f12bd3a11ec8857fbfeea9a1788b7b99cf0
Gourav Jain <72561022+gourav-007@users.noreply.github.com>
2024-06-01 23:18:33 +0530

    adding lat and long const

---
+++ src/main/java/com.assignment/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.assignment.constants;
+
+public class Constants {
+	  public static final double LAT_BEGIN = -40;
+    public static final double LONG_BEGIN = 5;
+    public static final double LAT_END = 5;
+    public static final double LONG_END = 100;
+}
