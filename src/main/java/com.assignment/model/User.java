5eb250a627f0ea5e2526a2bc5be041c6eaea9311
Gourav Jain <72561022+gourav-007@users.noreply.github.com>
2024-06-01 23:30:02 +0530

    Create User.java for Model

---
+++ src/main/java/com.assignment/model/User.java
@@ -0,0 +1,35 @@
+package com.assignment.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("address")
+    private Address address;
+
+    @JsonProperty("phone")
+    private String phone;
+
+    @JsonProperty("website")
+    private String website;
+
+    @JsonProperty("company")
+    private Company company;
+}
